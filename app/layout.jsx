ab5f1bf55d615bcc15ed091c7a041373d7621756
Kibiwott <bk.kibiwott@gmail.com>
2025-06-27 14:23:20 +0300

    Added tracking to the application

--- app/layout.jsx
+++ app/layout.jsx
@@ -18,7 +18,19 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      
+      <head>
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-VNYJM9G1R0"></script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-VNYJM9G1R0');
+            `,
+          }}
+        />
+      </head>
       <body className=
       {jetBrainsMono.variable}>
          <Header/>
